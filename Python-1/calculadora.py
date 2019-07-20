c384a5425ba40eb6973c721fcceb39296e360192
Marcos <marcos28_7@hotmail.com>
2019-07-20 13:08:31 +0200

    Calculadora
    Ok

--- Python-1/calculadora.py
+++ Python-1/calculadora.py
@@ -1,25 +1,32 @@
 print("\nBienvenido a la calculadora de Python\nAhora introduce los datos")
 
-# recogemos datos
-op = input ("Que cuenta quieres hacer (+ - * /):")
-num1 = float(input("Introduce primer valor: "))
-num2 = float(input("Introduce segundo valor: "))
-resultado = None
+op = None
+while op != "q":
+    # recogemos datos
+    print("Escribe q para salir")
+    op = input ("Que cuenta quieres hacer (+ - * / q):")
+    if op != "q":
+        num1 = float(input("Introduce primer valor: "))
+        num2 = float(input("Introduce segundo valor: "))
+    resultado = None
 
-# proceso de daatos
-if op == "+":
-    resultado = num1 + num2
-elif op == "-":
-    resultado = num1 - num2
-elif op == "*":
-    resultado = num1 * num2
-elif op == "/":
-    resultado = num1 / num2
-else:
-    print("No son datos validos")
-    exit()
+    # proceso de daatos
+    if op == "+":
+        resultado = num1 + num2
+    elif op == "-":
+        resultado = num1 - num2
+    elif op == "*":
+        resultado = num1 * num2
+    elif op == "/":
+        resultado = num1 / num2
+    elif op == "q":
+        print("Bye, bye!!!")
+        exit()
+    else:
+        print("No son datos validos!!!")
 
-# mostramos resultados
-print("El resultado de la operacion es: ")
-print(str(num1) + " " + op + " " + str(num2) + " " + str(resultado))
-print(resultado)
+
+    # mostramos resultados
+    print("El resultado de la operacion es: ")
+    print(str(num1) + " " + op + " " + str(num2) + " " + str(resultado))
+    print(resultado)
---
+++ Python-1/mood.py
@@ -0,0 +1,11 @@
+mood = input ("Como te sientes hoy? (feliz, nervioso, excitado):")
+sent = None
+
+if mood == "feliz":
+    print("Que bueno verte feliz!")
+elif mood == "nervioso":
+    print("Toma aire 3 veces!!!")
+elif mood == "excitado":
+    print("Relaja mirando el amancer")
+else:
+    print("Cheer up, mate!")
