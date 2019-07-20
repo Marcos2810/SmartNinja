b327ad70dab02d7edf477407822b758c1af3e2a5
Marcos <marcos28_7@hotmail.com>
2019-07-20 12:11:30 +0200

    py clase 1
    clase 1

---
+++ Python-1/main.py
@@ -0,0 +1,50 @@
+x = 5
+y = 7
+suma = x + y
+
+print(x + y)
+print(suma)
+print(str(x)+" + "+str(y))
+print(str(x)+" + "+str(y) + " " + "=" + " " + str(suma))
+
+#str-string convierte el numero a letra:  para que se muestre
+
+x = int(input("Tu numero x: "))
+y = int(input("Tu numero y: "))
+suma = x + y
+
+print(x + y)
+print(suma)
+print(str(x)+" + "+str(y))
+print(str(x)+" + "+str(y) + " " + "=" + " " + str(suma))
+
+#int-entero convierte la letra a numero: para que se muestre
+
+#float - decimales
+
+x = float(input("Tu numero x: "))
+y = float(input("Tu numero y: "))
+suma = x + y
+
+print(x + y)
+print(suma)
+print(str(x)+" + "+str(y))
+print(str(x)+" + "+str(y) + " " + "=" + " " + str(suma))
+
+some_string = "Hey there!"
+
+print(some_string)
+
+string_a = "Hello"
+string_b = "World!"
+
+print(string_a + string_b)
+print(string_a + " " + string_b)
+
+user_name = input("Please enter your name: ")
+
+print("Hello " + user_name + "!")
+
+first_num = int(input("Enter the first number: "))
+sec_num = int(input("Enter the first number: "))
+print(first_num + sec_num)
