23540451b7b95ffe5d21266f07b3432b1029fbc3
Marcos <marcos28_7@hotmail.com>
2019-07-28 15:28:41 +0200

    Domingo
    3

--- Python-1/guess_list.py
+++ Python-1/guess_list.py
@@ -2,10 +2,12 @@
 import random
 import json
 import datetime
+from operator import itemgetter
 
 
 with open("score_list.txt", "r") as score_file:
     score_list = json.loads(score_file.read())#json como lee lista Python - lee en formato lista
+
     for i in score_list:
         print(i)
     print("Top score (attempts): " + str(score_list[:5])) #[ 5 ] cantidad de numeros que mostrara
