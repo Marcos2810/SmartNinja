4180d7bfa6ad71485d55b402cac185ecd7cabe03
Marcos <marcos28_7@hotmail.com>
2019-08-10 13:34:18 +0200

    ok
    ok

--- Python-1/Clase 16/main.py
+++ Python-1/Clase 16/main.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from models import User, db
 
 app = Flask(__name__)
-
+db.create_all()
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
 
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
+    # create a User object
+    user = User(name=name, email=email)
+
+    # save the user object into a database
+    db.add(user)
+    db.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
