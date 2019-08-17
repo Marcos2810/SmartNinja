e4d523954522562f0ef6fa7c95da23adb41ee724
Marcos <marcos28_7@hotmail.com>
2019-08-17 09:43:48 +0200

    Clase 17
    ok

--- Python-1/Futbol/Football.py
+++ Python-1/Futbol/Football.py
@@ -10,12 +10,22 @@ class FootballPlayer():
         self.yellow_cards = yellow_cards
         self.red_cards = red_cards
 
-with open("foot.txt", "r") as player_foot:
-    player_list = json.loads(player_foot.read())
-    print(player_list)
+try:
+    with open("foot.txt", "r") as f:
+        player_list = json.loads(f.read())
+except:
+    player_list=[]
 
-print("tu jugador")
-jugadores=[]
+for jugadores in player_list:
+    print(jugadores)
+    print(jugadores['first_name'])#solo imprimo nombre - porque estan en diccionario
+
+print(player_list[:2])
+
+print(player_list)
+
+print("Tu jugador")
+
 name = input("Nombre: ")
 lastname = input("Apellido: ")
 height = float (input("Altura: "))
@@ -24,8 +34,8 @@ goals=int(input("Goles: "))
 y_cards=int(input("Tarjetas amarillas: "))
 r_cards=int(input("Tarjetas rojas: "))
 
-new_player = FootballPlayer(first_name=name, last_name=lastname, height_cm=float(height), weight_kg=float(weight),
-                            goals=int(goals), yellow_cards=int(y_cards), red_cards=int(r_cards))
-
-with open("foot.txt", "w") as player_foot:
-    player_foot.write(json.dumps(new_player.__dict__))
+new_player = FootballPlayer(first_name=name, last_name=lastname, height_cm=(height), weight_kg=(weight),
+                            goals=int(goals), yellow_cards=(y_cards), red_cards=int(r_cards))
+player_list.append(new_player.__dict__)
+with open("foot.txt", "w") as f:
+    f.write(json.dumps(player_list))
