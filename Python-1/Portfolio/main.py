db6a8e243d307dd208de19481b86a5771947fce7
Marcos <marcos28_7@hotmail.com>
2019-08-17 13:53:12 +0200

    ok
    17

--- Python-1/Portfolio/main.py
+++ Python-1/Portfolio/main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/about.html")
+@app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/portfolio.html")
+@app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
 
